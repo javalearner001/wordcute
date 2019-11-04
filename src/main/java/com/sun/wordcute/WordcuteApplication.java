63da4eb077c53147edcba3e2933251b3f7244497
sunkai <1403417950@qq.com>
2019-11-04 10:26:30 +0800

    有趣的单词项目搭建

---
+++ src/main/java/com/sun/wordcute/WordcuteApplication.java
@@ -0,0 +1,13 @@
+package com.sun.wordcute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcuteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordcuteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sun/wordcute/WordcuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.wordcute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcuteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
