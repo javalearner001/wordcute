31ad32bce42a287bbdc4e7591aaa466db37e7dde
sunkai <1403417950@qq.com>
2019-12-04 16:39:37 +0800

    服务器导出修改 4

--- src/main/java/com/sun/wordcute/controller/ExcelController.java
+++ src/main/java/com/sun/wordcute/controller/ExcelController.java
@@ -41,7 +41,7 @@ public class ExcelController {
             InputStream is = this.getClass().getClassLoader().getResourceAsStream("excel/new.xls");
             //InputStream is = new FileInputStream(template);
 
-            OutputStream os = new FileOutputStream("BOOT-INF/classes/excel/aaa.xls");
+            OutputStream os = new FileOutputStream("/data/new.xls");
 
             Map<String , Object> model=new HashMap<String , Object>();
             model.put("wordBases", wordBases);
@@ -60,7 +60,7 @@ public class ExcelController {
 
             //下面步骤为浏览器下载部分
             //指定数据生成后的文件输入流（将上述out的路径作为文件的输入流）
-            FileInputStream fileInputStream = new FileInputStream("BOOT-INF/classes/excel/aaa.xls");
+            FileInputStream fileInputStream = new FileInputStream("/data/new.xls");
 
             //设置下载头
             response.setHeader("content-Type", "application/vnd.ms-excel");
